c877103ee9b275bd9dfcd56867f809e5fa9ed8b5
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-05-25 01:54:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,10 @@
 
 
 def countdown(countdown)
-    while countdown > 0
-        puts "#{countdown} SECOND(S)"
-        countdown -= 1
-    end
+    while countdown >= -10 && countdown >= 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1 && countdown =
+  end
 end
 
 puts "HAPPY NEW YEAR!"
